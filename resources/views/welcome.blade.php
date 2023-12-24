77a72750af8d8ec029005a5e3a8a3334d7c5a3cc
Martin Calderon <elmcwl@gmail.com>
2023-12-23 18:24:07 -0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>Bienvenido</h1>
-<a href="/contacto">Contacto</a>
-<a href="{{ route('contacto') }}">Contacto (ruta con nombre)</a>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <h1>Bienvenido</h1>
+    <a href="/contacto">Contacto</a>
+    <a href="{{ route('contacto') }}">Contacto (ruta con nombre)</a>
+    </body>
 </html>
